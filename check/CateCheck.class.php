63efa10d879c44b81e43324e29239bf3a742e020
tnjaile <tnjaile@tn.edu.tw>
2019-11-12 14:57:14 +0800

    修正check目錄中$this->_flag非傳址變數，因此刪除

--- check/CateCheck.class.php
+++ check/CateCheck.class.php
@@ -1,22 +1,20 @@
 <?php
 //分類驗證類
-class CateCheck extends Check
-{
-    public function titleCheck(Model &$_model)
-    {
-        if (self::isNullString($_POST['cate_title'])) {
-            $this->_message[] = '標題不得為空';
-            $this->_flag      = false;
-        }
-        if (self::isNullArray($_POST['post_group'])) {
-            $this->_message[] = '發布群組不得為空';
-            $this->_flag      = false;
-        }
-        if (self::isNullArray($_POST['read_group'])) {
-            $this->_message[] = '讀取群組不得為空';
-            $this->_flag      = false;
-        }
-        return $this->_flag;
-    }
+class CateCheck extends Check {
+	public function titleCheck(Model &$_model) {
+		if (self::isNullString($_POST['cate_title'])) {
+			$this->_message[] = '標題不得為空';
+			return false;
+		}
+		if (self::isNullArray($_POST['post_group'])) {
+			$this->_message[] = '發布群組不得為空';
+			return false;
+		}
+		if (self::isNullArray($_POST['read_group'])) {
+			$this->_message[] = '讀取群組不得為空';
+			return false;
+		}
+		return true;
+	}
 
 }
--- check/NoticeCheck.class.php
+++ check/NoticeCheck.class.php
@@ -1,34 +1,32 @@
 <?php
 //公告驗證類
-class NoticeCheck extends Check
-{
-    public function allCheck(Model &$_model, array $_param)
-    {
+class NoticeCheck extends Check {
+	public function allCheck(Model &$_model, array $_param) {
 
-        if (self::isNullString($_POST['title'])) {
-            $this->_message[] = '標題不得為空';
-            $this->_flag      = false;
-        }
-        if (self::checkStrLength($_POST['title'], 2, 'min')) {
-            $this->_message[] = '標題不得小於2位！';
-            $this->_flag      = false;
-        }
-        if (self::checkStrLength($_POST['title'], 20, 'max')) {
-            $this->_message[] = '標題不得大於20位！';
-            $this->_flag      = false;
-        }
+		if (self::isNullString($_POST['title'])) {
+			$this->_message[] = '標題不得為空';
+			return false;
+		}
+		if (self::checkStrLength($_POST['title'], 2, 'min')) {
+			$this->_message[] = '標題不得小於2位！';
+			return false;
+		}
+		if (self::checkStrLength($_POST['title'], 20, 'max')) {
+			$this->_message[] = '標題不得大於20位！';
+			return false;
+		}
 
-        if ($_param['type'] == "url") {
-            if (self::isNullString($_POST['content'])) {
-                $this->_message[] = '網址不得為空';
-                $this->_flag      = false;
-            }
-            if (!self::checkUrl($_POST['content'])) {
-                $this->_message[] = '網址格式不正確';
-                $this->_flag      = false;
-            }
-        }
-        return $this->_flag;
-    }
+		if ($_param['type'] == "url") {
+			if (self::isNullString($_POST['content'])) {
+				$this->_message[] = '網址不得為空';
+				return false;
+			}
+			if (!self::checkUrl($_POST['content'])) {
+				$this->_message[] = '網址格式不正確';
+				return false;
+			}
+		}
+		return true;
+	}
 
 }

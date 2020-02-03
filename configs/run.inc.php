5af380b5f9177b633e87ae015b4020cd1dee05ea
tnjaile <tnjaile#tn.edu.tw>
2020-02-03 11:59:29 +0800

    改成不秀在導覽列

--- configs/run.inc.php
+++ configs/run.inc.php
@@ -17,11 +17,11 @@ if (strpos($_SERVER["SCRIPT_NAME"], '/admin/') !== false) {
 } else {
     $xoopsOption['template_main'] = basename(dirname(__DIR__)) . '_index.tpl';
     include_once '../../mainfile.php';
-    // die("index".(dirname($_SERVER["SCRIPT_NAME"]));
     include_once XOOPS_ROOT_PATH . "/header.php";
 }
 //引入系统配置文件
 include_once XOOPS_ROOT_PATH . "/modules/jillbase/profile.inc.php";
 //自動加載類
 include_once XOOPS_ROOT_PATH . "/modules/jill_notice/configs/autoload.inc.php";
-include_once NOTICE_DIR . "/interface_menu.php";
+// include_once NOTICE_DIR . "/interface_menu.php";
+include_once NOTICE_DIR . "/header.php";

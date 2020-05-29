d4faf2151f1fd372d321ac48e53deee31e1c7656
tnjaile <tnjaile#tn.edu.tw>
2020-05-29 16:54:09 +0800

    更新版本

--- xoops_version.php
+++ xoops_version.php
@@ -21,7 +21,7 @@ $modversion = array();
 
 //---模組基本資訊---//
 $modversion['name']        = _MI_JILLNOTICE_NAME;
-$modversion['version']     = '1.2';
+$modversion['version']     = '1.3';
 $modversion['description'] = _MI_JILLNOTICE_DESC;
 $modversion['author']      = _MI_JILLNOTICE_AUTHOR;
 $modversion['credits']     = _MI_JILLNOTICE_CREDITS;
@@ -31,8 +31,8 @@ $modversion['image']       = "images/logo.png";
 $modversion['dirname']     = basename(__DIR__);
 
 //---模組狀態資訊---//
-$modversion['status_version']      = '1.2';
-$modversion['release_date']        = '2020-03-13';
+$modversion['status_version']      = '1.3';
+$modversion['release_date']        = '2020-05-29';
 $modversion['module_website_url']  = 'https://github.com/tnjaile/';
 $modversion['module_website_name'] = _MI_JILLNOTICE_AUTHOR_WEB;
 $modversion['module_status']       = 'release';
